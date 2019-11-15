15d2d24316e67cc7aef0621237c7a75c98bdc1b5
Murasaki-1102 <c011729114@edu.teu.ac.jp>
2019-11-15 17:01:48 +0900

    add main()

---
+++ src/app/Main.java
@@ -0,0 +1,19 @@
+package app;
+
+class Main {
+
+	public static boolean end = false;
+
+	public static void main(String[] args) {
+		setDungion();
+
+		setSlime();
+
+		setEnemy();
+
+		setBasttleStatus();
+
+		displayGame();
+
+	}
+}
