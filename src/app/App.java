c9d47f7081f93227c64f831dcf1f693ba5018c76
C0117331 <c0117331c5@edu.teu.ac.jp>
2019-11-20 19:41:45 +0900

    try commit

--- src/app/App.java
+++ src/app/App.java
@@ -3,6 +3,6 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
-        System.out.println("kohei");
+        System.out.println("kohei oki");
     }
 }
