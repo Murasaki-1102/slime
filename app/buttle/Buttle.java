b664c9c4ec2e65e3450b0130ade49751cff48ae6
Mashu <c0117232fd@edu.teu.ac.jp>
2019-12-02 11:04:40 +0900

    方針の追加

--- app/buttle/Buttle.java
+++ app/buttle/Buttle.java
@@ -9,12 +9,22 @@ import actor.*;
 public class Buttle {
   public static boolean doButtle(Slime slime, Enemy enemy){
     boolean enemyDownFlg = false;
+    int count = 0;
+    int memory = 0;
     System.out.println("-----------------");
     System.out.println(enemy.getName() + "と出会った！");
     System.out.println("戦闘スタート！");
     do {
       ButtleContents.displayStatus(slime, enemy);
-      Janken.janken(slime, enemy);
+      if(enemy.getName() != "gate" || enemy.getName() != "hero"){
+        Janken.janken(slime, enemy);
+      }else{
+        memory = Janken.exJanken(slime,enemy,count,memory);
+        count++;
+        if(count > 3){
+          count = 0;
+        }
+      }
       if (enemy.getHitPoint() == 0) {
         enemyDownFlg = true;
         break;
--- app/buttle/Janken.java
+++ app/buttle/Janken.java
@@ -76,6 +76,76 @@ public class Janken {
     }
   }
 
+  //ボス戦（門番、勇者）用に特別に関数を用意する（中身はほぼ一緒）
+  public static int exJanken(Slime slime, Enemy enemy, int count, int memory) {
+    while (true) {
+      if (slime.getHitPoint() <= 0) {
+        System.out.println("死亡");
+        break;
+      } else if (enemy.getHitPoint() <= 0) {
+        System.out.println("バトルに勝利");
+        break;
+      }
+
+      Scanner scan = new Scanner(System.in);
+      int myHand = 0;
+      String name = enemy.getName();
+      try{
+        myHand = scan.nextInt();
+      }catch(Exception e){
+        System.out.print("正しい手をだしてね：");
+        continue;
+      }
+      switch (myHand) {
+        case 1:
+          System.out.println("自分....グー");
+          break;
+        case 2:
+          System.out.println("自分....チョキ");
+          break;
+        case 3:
+          System.out.println("自分....パー");
+          break;
+      }
+      int enemyHand = exRoutin(enemy.getName(),count,memory) + 1;
+
+      if(myHand == 1 || myHand == 2 || myHand == 3){
+        switch (enemyHand) {
+        case 1:
+          System.out.println("相手....グー");
+          break;
+        case 2:
+          System.out.println("相手....チョキ");
+          break;
+        case 3:
+          System.out.println("相手....パー");
+          break;
+        }
+      }
+      memory = myHand;
+      if (myHand == enemyHand) {
+        ButtleContents.draw(slime, enemy);
+      } else if (myHand == 1 && enemyHand == 2) {// グーとチョキ
+        ButtleContents.attack(slime.getAttack(), enemy, slime);
+      } else if (myHand == 1 && enemyHand == 3) {// グーとパー
+        ButtleContents.enemyAttack(enemy.getAttack(), slime, enemy);
+      } else if (myHand == 2 && enemyHand == 1) {// チョキとグー
+        ButtleContents.enemyAttack(enemy.getAttack(), slime, enemy);
+      } else if (myHand == 2 && enemyHand == 3) {// チ ョキとパー
+        ButtleContents.attack(slime.getAttack(), enemy, slime);
+      } else if (myHand == 3 && enemyHand == 1) {// パーとグー
+        ButtleContents.attack(slime.getAttack(), enemy, slime);
+      } else if (myHand == 3 && enemyHand == 2) {// パーとチョキ
+        ButtleContents.enemyAttack(enemy.getAttack(), slime, enemy);
+      } else {
+        System.out.print("正しい手をだしてね：");
+        continue;
+      }
+    }
+    
+    return memory;
+  }
+
   public static int routin(String name){
     int hand = 0;
     int checker = 0;
@@ -113,6 +183,18 @@ public class Janken {
       }
     }
 
+    return hand;
+  }
+
+  public static int exRoutin(String name,int count,int memory){
+    int hand = 0;
+    int checker = 0;
+    int random = new Random(3);
+    if(name == "gate"){
+      if(count == 0){
+
+      }
+    }
     return hand;
   }
 }
